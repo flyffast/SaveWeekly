1b22635ea8b25765fb8eacbbba43a8018c0d0514
flyffast <kitma.1186@gmail.com>
2023-04-01 03:08:32 -0700

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,38 @@
+# SaveWeekly
+SaveWeekly utilizies Spotify's API to create a copy of your Discover Weekly Songs.
+
+## Setup
+### (1) Install dependencies
+```bash
+pip install -r requirements.txt
+```
+### (2) Spotify API Credentials
+1. Open ```.env``` file on your local machine.
+2. Sign into your [Spotify API Dashboard](https://developer.spotify.com/dashboard) and create a new application. You can use any uri for the redirect uri, this is the base uri you will be redirected to once you have authorized the application to access your account. If you see "INVALID_CLIENT: Invalid redirect URI", then you have not added or used a valid uri as your redirect uri.
+3. Fill out the env file with the your Client ID, Client Secret details, Username located on your [Spotify Account Overview](https://www.spotify.com/account) and your Discover Weekly Playlist ID. Then save this file. **DO NOT SHARE THIS INFORMATION ANYHWERE PUBLICALLY**
+
+Example:
+
+```python
+CLIENT_ID = yourid
+CLIENT_SECRET = yoursecret
+USER_ID = yourusername
+DISCOVER_ID = yourdiscoverplaylistid
+```
+4. Execute main.py and open the URL genereated in your browswer.
+5. Authorize your app to access your Spotify account, this will then redirect you to your Redirect URI with a brand new url.
+6. Copy the whole url you were redirected to and paste the link into the console and hit enter, this will then create an authorization token as well as a refresh token. **Do not post your refresh token anywhere publically**
+
+```
+$python main.py
+
+Copy and paste the link that was opened here:
+> https://somelink.url/here?code=codehere
+
+Created Auth Token and Refresh Token
+```
+## Contributing
+Pull requests are welcome.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/main.py
@@ -0,0 +1,153 @@
+import requests
+import webbrowser
+import json
+from datetime import date
+from urllib.parse import urlencode
+import base64
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+clientID = os.getenv('CLIENT_ID')
+clientSecret = os.getenv('CLIENT_SECRET')
+encoded_credentials = base64.b64encode(clientID.encode() + b':' + clientSecret.encode()).decode("utf-8")
+user_id = os.getenv('USER_ID')
+discover_weekly_id = os.getenv('DISCOVER_ID')
+
+
+# URLS
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+BASE_URL = 'https://api.spotify.com/v1/'
+REDIRCT_URI = 'http://localhost:8888/callback'
+PLAYLIST_URL = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+DISCOVER_URL = f"http://api.spotify.com/v1/playlists/{discover_weekly_id}"
+
+class saveWeekly:
+    def __init__(self):
+        self.user_id = os.getenv('USER_ID')
+        self.spotify_token = ''
+        self.refresh_token = ''
+        self.tracks = ''
+        self.newPlaylist_id = ''
+
+    def getAuthCode(self, clientID):
+        # POST
+        auth_code = {
+            'client_id': clientID,
+            'response_type': 'code',
+            'redirect_uri': REDIRCT_URI,
+            'scope': 'user-library-read playlist-read-private playlist-modify-public playlist-modify-private'
+        }
+        webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_code))
+
+    def getAccessToken(self):
+        codeLink = input('Copy and paste the link that was opened here: ')
+        print(f"This is the link that was pasted: {codeLink}")
+        code = codeLink.split('code=')[-1]
+        access_param = {
+            'grant_type' : 'authorization_code',
+            'code' : code,
+            'redirect_uri': REDIRCT_URI
+        }
+
+        headers = {
+            'Authorization' : 'Basic ' + encoded_credentials,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        response = requests.post(TOKEN_URL, data = access_param, headers= headers)
+        self.spotify_token = response.json()['access_token']
+        self.refresh_token = response.json()['refresh_token']
+        if response.status_code == 200:
+            print(f"The Access Token : {self.spotify_token}\nThe Refresh Token : {self.refresh_token}")
+        else:
+            print("Invalid Authorization Code, please makes sure you approve the use of SaveWeekly")
+    def findPlaylist(self):
+        print("Finding your discover weekly songs")
+        headers = {
+            'Content-Type' : 'applicaiton/json',
+            'Authorization' : f"Bearer {self.spotify_token}"
+        }
+
+        response = requests.get(DISCOVER_URL, headers= headers)
+        response_json = response.json()
+        for song in response_json['tracks']["items"]:
+            self.tracks += (song['track']['uri'] + ',')
+        self.tracks = self.tracks[:-1]
+        if response.status_code == 200:
+            print('Found playlist!')
+            self.addToPlaylist()
+        elif response.status_code == 401:
+            print("Unable to find playlist token has expired or revoked")
+        elif response.status_code == 403:
+            print('Bad OAuth request')
+        else:
+            print('The app has exceeded its rate limits.')
+    
+    def createPlaylist(self):
+        print('Creating playlist...')
+        today = date.today()
+        playlist_data = json.dumps({
+            'name' : f"{today}'s Discover Weekly",
+            'description' : 'Discover weekly recorded by SaveWeekly Bot',
+            'public' : False
+        })
+        headers = {
+            'Content-Type' : 'applicaiton/json',
+            'Authorization' : f"Bearer {self.spotify_token}"
+        }
+        response = requests.post(PLAYLIST_URL, data = playlist_data, headers = headers)
+        if response.status_code == 201:
+            print('Created playlist!')
+        elif response.status_code == 401:
+            print("Unable to find playlist token has expired or revoked")
+        elif response.status_code == 403:
+            print('Bad OAuth request')
+        else:
+            print('The app has exceeded its rate limits.')
+        return response.json()['id']
+    
+    def addToPlaylist(self):
+        print("Adding songs...")
+        self.newPlaylist_id = self.createPlaylist()
+        NEWPLAYLIST_URL = f"https://api.spotify.com/v1/playlists/{self.newPlaylist_id}/tracks?uris={self.tracks}"
+        headers = {
+            'Content-Type' : 'applicaiton/json',
+            'Authorization' : f"Bearer {self.spotify_token}"
+        }
+        response = requests.post(NEWPLAYLIST_URL, headers = headers)
+        if response.status_code == 201:
+            print("Added Songs!")
+        elif response.status_code == 401:
+            print("Unable to find playlist token has expired or revoked")
+        elif response.status_code == 403:
+            print('Bad OAuth request')
+        else:
+            print('The app has exceeded its rate limits.')
+
+
+    def refreshAccessToken(self):
+        refresh_data = {
+            'grant_type' : 'refresh_token',
+            'refresh_token' : self.refresh_token
+        }
+        headers = {
+            'Authorization' : 'Basic ' + encoded_credentials,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        response = requests.post(TOKEN_URL, data = refresh_data, headers = headers)
+        self.spotify_token = response.json()['access_token']
+        if response.status_code == 200:
+            print('Token has been refreshed')
+        else:
+            print('Token failed to refresh')
+if __name__ == '__main__':
+    s = saveWeekly()
+    s.getAuthCode(clientID)
+    s.getAccessToken()
+    s.findPlaylist()
+
+
